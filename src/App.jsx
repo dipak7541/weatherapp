d07eb971ef3eaeb8331ac94b5c484129c30eaa59
dipak7541 <tiwaritanka75@gmail.com>
2021-06-11 15:05:32 +0545

    React App for Weather update of different cities using search filter input

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TempApp from './Compoents/TempApp';
+
+const App =() =>{
+    return(
+        <TempApp />
+    )
+}
+export default App;
---
+++ src/Compoents/TempApp.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import './css/style.css';
+
+const TempApp = () => {
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("Butwal");
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a61487d7ad71d409e6100ed013f2338e`
+            const response = await fetch(url);
+            const resJson = await response.json()
+            setCity(resJson.main)
+        }
+        fetchApi();
+    }, [search])
+
+
+    return (
+        <>
+            <div className="box">
+                <div className="inputData">
+                    <input
+                        type="search"
+                        value={search}
+                        className="inputField"
+                        onChange={(event) => {
+                            setSearch(event.target.value)
+                        }}
+                    />
+                </div>
+                {
+                    !city ? (
+                        <p className="errorMsg">No Data Found</p>
+                    ) : (
+                        <div>
+                            <div className="info">
+                                <h2 className="location">
+                                    <i className="fas fa-street-view"></i>{search}
+                                </h2>
+                                <h1 className="temp">{city.temp}</h1>
+                                <h3 className="tempmin_max">Minimum:{city.temp_min}| Maximum:{city.temp_max} </h3>
+                            </div>
+
+                            <div className="wave -one"></div>
+                            <div className="wave -two"></div>
+                            <div className="wave -three"></div>
+                        </div>
+                    )}
+
+
+            </div>
+
+        </>
+    )
+
+};
+
+export default TempApp;
